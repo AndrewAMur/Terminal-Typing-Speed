c5d45e0d80b2b671b4d0a7fbcb75684a0bd57dba
Andrew Muratov <andrewamuratov@gmail.com>
2024-08-05 16:30:23 -0400

    input validation

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 TEXT="The quick brown fox jumps over the lazy dog"
-
-echo -ne "$TEXT\r"
+echo -e "\nType the following text as quickly and accurately as you can:\n"
+echo -e "\033[33m$TEXT\033[0m\n"
 
 tput civis
 
@@ -23,7 +23,7 @@ while [ $pos -lt ${#TEXT} ]; do
                 ((pos--))
                 typed="${typed%?}"
             fi
-        else
+        elif [ "$char" = "${TEXT:$pos:1}" ]; then
             typed+="$char"
             ((pos++))
         fi
@@ -45,4 +45,9 @@ elapsed=$(echo "$end_time - $start_time" | bc)
 words=$(echo "$typed" | wc -w)
 wpm=$(echo "scale=2; $words / ($elapsed / 60)" | bc)
 
+# Calculate accuracy
+correct_chars=$(comm -12 <(echo "$TEXT" | fold -w1) <(echo "$typed" | fold -w1) | wc -l)
+accuracy=$(echo "scale=2; $correct_chars / ${#TEXT} * 100" | bc)
+
 echo -e "\n\nYour typing speed is: $wpm WPM"
+echo -e "Your accuracy is: $accuracy%"
